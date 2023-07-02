9cb158b5aaf657b19d715505ce7afc37f9dfdcc9
GOLLAVILLISANJAYKUMAR <sanjaykumargollavilli@gmail.com>
2023-07-02 11:06:57 +0530

    Update 2.0  files with new changes

---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+# gunicorn_config.py
+
+bind = '0.0.0.0:5000'  # Specify the address and port to bind the server to
+workers = 4  # Set the number of worker processes
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+# from helper import app
+# from users.routes import users_bp
+# from waitress import serve
+
+# app.secret_key = 'your_secret_key'
+
+# app.register_blueprint(users_bp, url_prefix='/')
+
+# mode=''
+# if __name__=='__main__':
+#     if mode=='dev':
+#         app.run(debug=True)
+#     else:
+#         serve(app,host='0.0.0.0',port=50000,threads=4, url_prefix='/')
+
 from helper import app
 from users.routes import users_bp
-from waitress import serve
+from gunicorn.app.base import BaseApplication
 
 app.secret_key = 'your_secret_key'
-
 app.register_blueprint(users_bp, url_prefix='/')
 
-mode=''
-if __name__=='__main__':
-    if mode=='dev':
-        app.run(debug=True)
-    else:
-        serve(app,host='0.0.0.0',port=50000,threads=4, url_prefix='/')
+class FlaskApplication(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        for key, value in self.options.items():
+            self.cfg.set(key, value)
+
+    def load(self):
+        return self.application
+
+if __name__ == '__main__':
+    options = {
+        'bind': '0.0.0.0:5000',
+        'workers': 4
+    }
+
+    FlaskApplication(app, options).run()
