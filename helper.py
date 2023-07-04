4d4d7ed942e1cf46a2e88f377d942ba2fc773d20
GOLLAVILLISANJAYKUMAR <sanjaykumargollavilli@gmail.com>
2023-07-04 11:56:58 +0530

    changed postgres to postgresql

--- helper.py
+++ helper.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgressql://milletsaga_user:PNUr51qtkmMYsFczfxJfU2NexgFHz0sO@dpg-cih4kedgkuvojj96qod0-a/milletsaga'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://milletsaga_user:PNUr51qtkmMYsFczfxJfU2NexgFHz0sO@dpg-cih4kedgkuvojj96qod0-a/milletsaga'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Regtb(db.Model):
+class regtb(db.Model):
     __tablename__ = 'regtb'
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
